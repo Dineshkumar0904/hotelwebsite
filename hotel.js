0bc25da5c3c9a242888d1626d92eb06051e00e75
Dineshkumar0904 <148203603+Dineshkumar0904@users.noreply.github.com>
2023-11-05 09:20:48 +0530

    Add files via upload

---
+++ hotel.js
@@ -0,0 +1,13 @@
+var swiper = new swiper(".home-slider", {
+    grabCursor:true,
+    loop:true,
+    centeredSlides:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
